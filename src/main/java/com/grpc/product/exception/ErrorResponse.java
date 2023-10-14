a833997d4d0ab0d80249363870d3a8ec61a5fb5b
Nedim Kaçan <nedimkacanofficial@gmail.com>
2023-10-14 17:36:48 +0300

    Exception classes were created.

---
+++ src/main/java/com/grpc/product/exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.grpc.product.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private List<String> details;
+    private LocalDateTime dateTime;
+}
